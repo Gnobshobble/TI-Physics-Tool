adb557eb496d195d119fc12654ae654d904f266a
Gnobshobble <98632087+Gnobshobble@users.noreply.github.com>
2022-12-13 10:10:38 -0800

    has bugs, add menuing

--- main.py
+++ main.py
@@ -2,11 +2,16 @@
 menu_options = {1: 'Atwoods', 2: 'Projectiles', 3: 'Newtonian Equations', 4: 'Energy', 5: 'Power and Work'}
 
 # print the dictionary keys and entries by line so the user can select an option
-for i, option in enumerate(menu_options):
-    print(f'{i}: {option}')
+for option_key in menu_options:
+    print(f'{option_key}: {menu_options[option_key]}')
 
 # get user input
-input = input('')
+def get_user_menu_option():
+    inp = -1
+    while inp not in menu_options_keys:
+        inp = int(input('Select an option: '))
+    return inp
+selection = get_user_menu_option()
 
 # Atwoods
 # projectile motion
