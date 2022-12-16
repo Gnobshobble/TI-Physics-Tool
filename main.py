8aac84785e9e7c1de24140ba2803e3152f574602
Gnobshobble <98632087+Gnobshobble@users.noreply.github.com>
2022-12-16 14:06:33 -0800

    changed to a nested dictionary based approach for the menuing and indexing

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# initalize a dictionary of menu options for navigation. Indexing to avoid clunky user input 
-menu_options = {1: 'Atwoods', 2: 'Projectiles', 3: 'Newtonian Equations', 4: 'Energy', 5: 'Power and Work'}
+# initalize a dictionary of menu options containg subdictionaries. Indexing to avoid clunky user input. 
+# subdictionaries act as sudo objects, containing varyable feilds
+menu_options = {1: {'name': 'atwoods'}, 
+2: {'name': 'projectiles'}, 
+3: {'name': 'newtonian equations'}, 
+4: {'name': 'energy'}, 
+5: {'name': 'power and work'}}
 
 # print the dictionary keys and entries by line so the user can select an option
 for option_key in menu_options:
-    print(f'{option_key}: {menu_options[option_key]}')
+    print(f'{option_key}: {menu_options[option_key]["name"]}')
 
 # get user input
 def get_user_menu_option():
     inp = -1
-    while inp not in menu_options_keys:
+    while inp not in list(menu_options.keys()):
         inp = int(input('Select an option: '))
-    return inp
+    return menu_options[inp]["name"]
 selection = get_user_menu_option()
+
+print(selection)
 
 # Atwoods
 # projectile motion
